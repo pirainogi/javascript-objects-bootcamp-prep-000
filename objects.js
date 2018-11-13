67aac2174c392f68abc09a5b259076a67dc437f4
Gabbie Piraino <pirainogi@gmail.com>
2018-11-13 01:58:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ const playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+  return Object.assign({}, playlist, { [artistName]: songTitle} );
 }
 
 
